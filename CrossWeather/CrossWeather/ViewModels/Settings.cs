cc771b9d1df4a78ac5f0c0aeb011725a358aed04
Kerego <kerego.aka.nillan@gmail.com>
2015-04-07 20:46:16 +0300

    major fixes to geocoding

---
+++ CrossWeather/CrossWeather/ViewModels/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossWeather
+{
+    class Settings
+    {
+        public bool useGeoLocation { get; set;}
+
+        public Settings()
+        {
+            useGeoLocation = false;
+        }
+    }
+}
