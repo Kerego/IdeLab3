02a4dff5d678c8fbaef80365362d9359c4607702
Kerego <kerego.aka.nillan@gmail.com>
2015-05-13 19:48:05 +0300

    Settings Page

---
+++ CrossWeather/CrossWeather/Views/SettingsPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+
+namespace CrossWeather
+{
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+			CityData.Text = UserPrefSettings.Location;
+
+			grid.Padding = new Thickness (0, 0, 50, 0);
+
+			if (Device.OS == TargetPlatform.Android)
+				grid.Padding = new Thickness (20, 0, 20, 0);
+
+			SaveButton.Clicked += (s, e) => {
+				UserPrefSettings.Location = CityData.Text;
+			};
+			GPSButton.Clicked += async (sender, e) => {
+				CityData.Text = await ApiHelper.getLocation();
+			};
+
+        }
+    }
+}
